902fa2610b80fd293ebc43235acb549fff003858
akaryaka <a.karyaka.2dit@gmail.com>
2024-04-05 22:47:23 +0600

    3.4

--- test.js
+++ test.js
@@ -1,2 +1,41 @@
 "use strict";
-const a = 5;
+let a = 4;
+let b = 'string';
+let d = ['a', 'b', 'c'];
+let e = 3;
+e = 'string e';
+// фунцкия test принимает значение в виде строки и возвращает строку
+function test(a) {
+    return '';
+}
+// для разных возвращаемых значений
+function test2(a) {
+    return '';
+}
+// если функция ничего не возвращает
+const test3 = (a) => {
+    return;
+};
+// пример с массивом
+d = d.map((x) => x.toUpperCase());
+console.log(d);
+// long?:number замена записи long: number | undefined
+function countCoord(coord) {
+}
+// проверка типов
+function printIt(id) {
+    if (typeof id === 'number') {
+        console.log(id.toString());
+    }
+    else if (typeof id === 'string') {
+        console.log(id.toUpperCase());
+    }
+}
+// проверка на массив
+function getSum(a) {
+    // проверяем, что a - массив
+    if (Array.isArray(a)) {
+    }
+}
+const x = undefined;
+const z = null;
--- test.ts
+++ test.ts
@@ -1,1 +1,50 @@
-const a: number = 5;
+let a: number = 4;
+let b: string = 'string';
+
+let d: string[] =  ['a', 'b', 'c'];
+
+let e: any = 3;
+e = 'string e';
+
+// фунцкия test принимает значение в виде строки и возвращает строку
+function test(a: string): string {
+  return '';
+}
+
+// для разных возвращаемых значений
+function test2(a: string): number | string {
+  return '';
+}
+
+// если функция ничего не возвращает
+const test3 = (a: number): void => {
+  return ;
+}
+
+// пример с массивом
+d = d.map((x: string) => x.toUpperCase());
+console.log(d);
+
+// long?:number замена записи long: number | undefined
+function countCoord(coord: {lat: number, long?: number}) {
+
+}
+
+// проверка типов
+function printIt(id: number | string ) {
+  if(typeof id === 'number') {
+    console.log(id.toString());
+  } else if(typeof id === 'string') {
+    console.log(id.toUpperCase());
+  }
+}
+
+// проверка на массив
+function getSum(a: number | number[]) {
+  // проверяем, что a - массив
+  if(Array.isArray(a)) {
+  }
+}
+
+const x: undefined = undefined;
+const z: null = null;
