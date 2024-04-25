28a04e1a7ce7e2efd4a2d7a37e9fe7bb7a635051
akaryaka <akaryaka1997@gmail.com>
2024-04-25 17:40:54 +0600

    3.7 begin

--- 3.7/literal.ts
+++ 3.7/literal.ts
@@ -1,1 +1,16 @@
-console.log('literal');
+// в таком исполнении пользы нет
+let c: 'test' = 'test'; 
+
+// покажем другой пример
+
+type actionType = 'up' | 'down';
+
+// ошибка
+function performeAction(action: actionType): 1 | 2 {
+  switch(action) {
+    case 'down':
+      return 1;
+      console.log('up');
+  }
+  // return 1;
+}
