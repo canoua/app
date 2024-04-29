f12c71612ba31e728b1a905c3d4213154f87012a
akaryaka <a.karyaka.2dit@gmail.com>
2024-04-30 01:18:04 +0600

    3.8 begin

---
+++ 3.8/enum.ts
@@ -0,0 +1,37 @@
+enum Direction {
+  up = 1,
+  // down = 2
+  down
+}
+
+enum DirectionString {
+  up = 'up',
+  down = 'down',
+  right = 'right',
+  left = 'left'
+}
+
+enum Decision {
+  yes = 1,
+  no = calcEnum()
+}
+
+// может возвращать только число
+function calcEnum() {
+  return 2;
+}
+
+enum Test {
+  A
+}
+
+let testVar = Test.A;
+let nameA = Test[testVar];
+
+// выведет 0
+console.log(testVar);
+
+// выведет А
+console.log(nameA);
+
+
