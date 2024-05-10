c95c8b83c581a00227d5854b9451eab31e900f9c
akaryaka <a.karyaka.2dit@gmail.com>
2024-05-10 23:48:54 +0600

    3.10 - need practice

--- 3.10/generics.ts
+++ 3.10/generics.ts
@@ -1,4 +1,4 @@
-// <T> - geerics type, который мы можель использовать в аргументе
+// <T> - geerics type, который мы можем использовать в аргументе
 
 function logTime<T>(val: T): T {
   console.log(new Date());
